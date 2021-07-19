a7e287ee0f497bd69f66ff543dcfdc8b7384dfc1
avnoks <ghuwrov@the-huangs.net>
2021-07-19 10:48:26 -0700

    Added usage examples and fixed addressing documentation in start functions.
    Usage examples are commented-out code at bottom of header file.
    Clarified that I2C addresses supplied to start functions are supposed
    to be 7-bit addresses and that read/write bit is automatically set
    depending on transaction type.

--- lib/include/i2c.h
+++ lib/include/i2c.h
@@ -18,8 +18,8 @@ void i2c_init();
 /*
  * Start an I2C transaction.
  * Arguments  
- *   addr : 8-bits.  Bits 7-1 are I2C address.  Bit 0 indicates 
- *     read/write (0 -> write, 1 -> read).  
+ *   addr : 7-bit I2C address.  Automagically sets appends read/write bit
+ *     depending on the type argument.
  *   type : Indicates type of I2C transaction; signed 16-bit integer.  
  *     0  -> I2C write transaction.  Write byte with i2c_write().
  *       Transaction macro: I2C_WRITE
@@ -30,18 +30,23 @@ void i2c_init();
  *     [1 -- 32767] -> Read transaction of definite length.  Number of 
  *       bytes to read is argument value [1 -- 32767].  
  *       Bytes must be read using i2c_read_auto().
+ * Returns 
+ *   true if start was sucessful; false if something went wrong.
  */
 bool i2c_start(uint8_t addr, int16_t type);
 
-/* Restart an I2C transaction.  Arguments are the same as i2c_start(). */
+/* Restart an I2C transaction.  Arguments and return-value are the same 
+ * as i2c_start(). */
 bool i2c_restart(uint8_t addr, uint16_t type);
 /* Finish an I2C transaction. */
 void i2c_stop();
 
-/* Write a byte to I2C device in ongoing transaction. */
+/* Write a byte to I2C device in ongoing transaction.  Returns true 
+ * if device ACKnowledges write; returns false if N ACKnowledge was 
+ * detected. */
 bool i2c_write(uint8_t byte);
 
-/* Yes, there are three different read functions.  Enjoy. */
+/* Yes, there are three different single-byte read functions.  Enjoy. */
 
 /* 
  * Read a byte in an I2C read transaction of unspecified length.
@@ -78,5 +83,49 @@ uint8_t i2c_read_auto();
  * to I2C device in ongoing transaction */
 i2c_size_t i2c_write_buffer(uint8_t *buffer, i2c_size_t length);
 /* Read length bytes from I2C device to an array of unsigned bytes 
- * in ongoing transaction */
+ * in ongoing transaction.  Transaction should be opened with using 
+ * I2C_READ as type argument to i2c_start()/i2c_restart(). */
 i2c_size_t i2c_read_to_buffer(uint8_t *buffer, i2c_size_t length);
+
+
+
+/* **********************************************************************
+ * Usage examples
+ */
+
+/* 
+
+uint8_t addr = 0x5A;     // Arbitrary made-up address
+
+i2c_init();  
+
+// Write two bytes  
+i2c_start(addr, I2C_WRITE);  
+i2c_write(0xA2);  // Optionally, use return-value to see if write was sucessful.
+i2c_write(0xF0);  
+i2c_stop();  
+
+// Read a few bytes in a transaction whose length is not specified at the start.
+i2c_start(addr, I2C_READ);
+uint8_t byte1 = i2c_read();  
+uint8_t byte2 = i2c_read();  
+uint8_t byte_last = i2c_read_last();  
+i2c_stop();  
+
+// Read a few bytes in a transaction whose length *is* specified at the start.  
+i2c_start(addr, 5);
+for (uint8_t i = 0; i < 5; i++) { 
+  process(i2c_read_auto());
+}
+i2c_stop();
+
+// Buffer examples
+uint8_t buffer[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 10};  // BUT WHAT HAPPENED TO 9?  
+i2c_start(addr, I2C_WRITE);  
+i2c_write_buffer(buffer, 10);  
+i2c_stop();  
+
+i2c_start(addr, I2C_READ);  
+i2c_read_buffer(buffer, 10);  
+i2c_stop();  
+ */
